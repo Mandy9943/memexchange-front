1defd33fa2d4ed766e7aec462a3e12c14124b7a1
Mandy9943 <armandoc9943@gmail.com>
2025-01-06 18:28:50 -0300

    Enhance ActionCard component with new routing logic for task actions
    - Added routing functionality to the ActionCard component for specific tasks: 'Create a MemeCoin' and 'Buy MemeCoin'.
    - Integrated useRouter from Next.js to facilitate navigation based on task titles.
    - Improved user experience by enabling direct navigation to relevant pages upon task selection.

--- src/app/components/ActionCard.tsx
+++ src/app/components/ActionCard.tsx
@@ -3,17 +3,28 @@ import { RequireAuth } from '@/components/ConnectButton/RequireAuth';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { useAppSelector } from '@/hooks/useRedux';
+import { RouteNamesEnum } from '@/localConstants';
 import { selectUserAddress } from '@/redux/dapp/dapp-slice';
 import { rewardService, Task } from '@/services/rest/backendApi/reward';
 import Cookies from 'js-cookie';
+import { useRouter } from 'next/navigation';
 import toast from 'react-hot-toast';
 const ActionCard = ({ task }: { task: Task }) => {
+  const router = useRouter();
   const userAddress = useAppSelector(selectUserAddress);
 
   const handleMakeTask = async () => {
     if (task.title === 'Connect Wallet') {
       window.location.reload();
     }
+    if (task.title === 'Create a MemeCoin') {
+      router.push(RouteNamesEnum.createCoin);
+    }
+
+    if (task.title === 'Buy MemeCoin') {
+      router.push(RouteNamesEnum.memeCoins);
+    }
+
     if (task.type === 'link') {
       window.open(task.linkUrl, '_blank');
       toast.promise(
