4d1a7e8b8bb812da13cd4f5a7beb5b3f6bc4b2be
Mandy9943 <armandoc9943@gmail.com>
2025-01-07 13:13:11 -0300

    change meme-coin to client component

--- src/app/meme-coins/components/MemeCoins/MemeCoins.tsx
+++ src/app/meme-coins/components/MemeCoins/MemeCoins.tsx
@@ -1,50 +1,55 @@
 import { Card } from '@/components/ui/card';
-import { BondingPair } from '@/types/bondingPairsTypes';
+import { getBondingPairs } from '@/services/rest/backendApi/bonding-pair';
 import Image from 'next/image';
 import Link from 'next/link';
+import useSWR from 'swr';
 import Header from '../Header';
 import BondingCurveProgress from './components/BondingCurveProgress';
 import MarketCap from './components/MarketCap';
 
-function MemeCoins({ bondingPairs }: { bondingPairs: BondingPair[] }) {
+function MemeCoins() {
+  const { data: bondingPairs } = useSWR('/bonding-pairs', getBondingPairs);
+
   return (
     <div className=''>
       <Header />
 
-      <div className='flex flex-col gap-8'>
-        {bondingPairs.map((coin, idx) => (
-          <Link href={`/meme-coins/${coin.address}`} key={idx}>
-            <Card
-              key={idx}
-              className='bg-neutral-800 border border-neutral-700 p-4 hover:bg-neutral-700 transition-all'
-            >
-              <div className='flex items-center gap-4'>
-                {coin.coin && (
-                  <Image
-                    src={coin.coin?.imageUrl}
-                    alt={coin.coin?.name}
-                    width={100}
-                    height={100}
-                  />
-                )}
-                <div className='flex flex-1 text-white flex-col'>
-                  <div>
-                    <p className='text-lg font-semibold'>{coin.coin?.name}</p>
-                  </div>
-                  <div className='mb-1'>
-                    <MarketCap bondingAddress={coin.address} />
-                  </div>
-                  <BondingCurveProgress bondingAddress={coin.address} />
+      {bondingPairs && bondingPairs.length > 0 && (
+        <div className='flex flex-col gap-8'>
+          {bondingPairs.map((coin, idx) => (
+            <Link href={`/meme-coins/${coin.address}`} key={idx}>
+              <Card
+                key={idx}
+                className='bg-neutral-800 border border-neutral-700 p-4 hover:bg-neutral-700 transition-all'
+              >
+                <div className='flex items-center gap-4'>
+                  {coin.coin && (
+                    <Image
+                      src={coin.coin?.imageUrl}
+                      alt={coin.coin?.name}
+                      width={100}
+                      height={100}
+                    />
+                  )}
+                  <div className='flex flex-1 text-white flex-col'>
+                    <div>
+                      <p className='text-lg font-semibold'>{coin.coin?.name}</p>
+                    </div>
+                    <div className='mb-1'>
+                      <MarketCap bondingAddress={coin.address} />
+                    </div>
+                    <BondingCurveProgress bondingAddress={coin.address} />
 
-                  <p className='text-sm text-muted-foreground'>
-                    {coin.address}
-                  </p>
+                    <p className='text-sm text-muted-foreground'>
+                      {coin.address}
+                    </p>
+                  </div>
                 </div>
-              </div>
-            </Card>
-          </Link>
-        ))}
-      </div>
+              </Card>
+            </Link>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
--- src/app/meme-coins/page.tsx
+++ src/app/meme-coins/page.tsx
@@ -1,14 +1,11 @@
+'use client';
 import { Card } from '@/components/ui/card';
-import { getBondingPairs } from '@/services/rest/backendApi/bonding-pair';
 import MemeCoins from './components/MemeCoins/MemeCoins';
 
-const MemeCoinsPage = async () => {
-  const memeCoins = await getBondingPairs();
-  console.log(memeCoins);
-
+const MemeCoinsPage = () => {
   return (
     <Card className='bg-[#1e222d] w-full max-w-6xl mx-auto py-6 px-8'>
-      <MemeCoins bondingPairs={memeCoins} />
+      <MemeCoins />
     </Card>
   );
 };
