f3472762b18905a7221c463a38d8d48b8cf479eb
Mandy9943 <armandoc9943@gmail.com>
2025-01-11 16:28:49 -0300

    Refactor ActionCards component to use SWR for data fetching
    - Replaced the async function with a hook-based approach using SWR for fetching tasks.
    - Added loading and no tasks found states to improve user experience.
    - Enhanced code readability and maintainability by leveraging SWR's built-in features.

--- src/app/components/ActionCards.tsx
+++ src/app/components/ActionCards.tsx
@@ -1,9 +1,16 @@
+'use client';
 import { rewardService } from '@/services/rest/backendApi/reward';
+import useSWR from 'swr';
 import ActionCard from './ActionCardContainer';
 
-const ActionCards = async () => {
-  const tasks = await rewardService.getAllTasks();
-  console.log(tasks);
+const ActionCards = () => {
+  const { data: tasks, isLoading } = useSWR(
+    '/api/tasks',
+    rewardService.getAllTasks
+  );
+
+  if (isLoading) return <div>Loading...</div>;
+  if (!tasks) return <div>No tasks found</div>;
 
   return (
     <div className='mt-8 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4'>
