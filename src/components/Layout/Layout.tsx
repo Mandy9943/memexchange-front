681901a47a1878543fc29ce87c7ad243b75f200b
Mandy9943 <armandoc9943@gmail.com>
2025-01-26 02:36:40 -0300

    refactor(Layout): comment out devnet banner in Layout.tsx

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -6,9 +6,9 @@ import { Nav } from './Nav';
 export const Layout = ({ children }: PropsWithChildren) => {
   return (
     <div className='flex flex-col min-h-screen mb-20'>
-      <div className='bg-yellow-500 text-black text-center py-1 font-medium'>
+      {/* <div className='bg-yellow-500 text-black text-center py-1 font-medium'>
         ⚠️ Running on Devnet
-      </div>
+      </div> */}
 
       <main className='flex flex-grow items-stretch justify-center p-8 flex-1'>
         <Header />
