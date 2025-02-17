79ad4450144a62937f993165e4200085b3cab729
Mandy9943 <armandoc9943@gmail.com>
2025-02-17 13:11:55 -0300

    fix(MemeCoins): Update environment check for bonding address

--- src/app/meme-coins/const.ts
+++ src/app/meme-coins/const.ts
@@ -1,7 +1,6 @@
 import { environment } from '@/config';
-import { EnvironmentsEnum } from '@multiversx/sdk-dapp/types';
 
 export const trendingBondingAddress =
-  environment === EnvironmentsEnum.devnet
+  (environment as unknown) === 'devnet'
     ? 'erd1qqqqqqqqqqqqqpgq784lvgns80an5qp62khwgw6fjc4u5vy3pl6sylujk7'
     : 'erd1qqqqqqqqqqqqqpgqus5p5gh09p43g8ng9qx0mw00g2z8cc38pl6smg8jk8';
