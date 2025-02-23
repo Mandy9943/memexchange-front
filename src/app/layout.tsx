9329099947e2e310757f3509614c2d26ba4e4bf0
Mandy9943 <armandoc9943@gmail.com>
2025-02-22 23:56:04 -0300

    feat(Analytics): Add Twitter pixel tracking script to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -76,6 +76,14 @@ export default function RootLayout({ children }: { children: ReactNode }) {
           data-key='ISj68cQcHMuy65A48pNxPg'
           strategy='afterInteractive'
         />
+        <Script id='twitter-pixel' strategy='afterInteractive'>
+          {`
+            !function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
+            },s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',
+            a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
+            twq('config','p6587');
+          `}
+        </Script>
       </head>
       <body className={`${inter.className} bg-neutral-900 text-white`}>
         <NextSSRPlugin
