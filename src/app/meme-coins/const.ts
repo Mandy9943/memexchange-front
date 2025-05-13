c4da2fd29a511ad9c17ad1ea679f782fe8d2dacf
Mandy9943 <armandoc9943@gmail.com>
2025-05-12 21:18:39 -0300

    fix(MemeCoins): Update bonding address for mainnet environment

--- src/app/meme-coins/const.ts
+++ src/app/meme-coins/const.ts
@@ -3,4 +3,4 @@ import { environment } from '@/config';
 export const trendingBondingAddress =
   (environment as unknown) === 'devnet'
     ? 'erd1qqqqqqqqqqqqqpgq784lvgns80an5qp62khwgw6fjc4u5vy3pl6sylujk7'
-    : 'erd1qqqqqqqqqqqqqpgq8n7gkr2pc0wm5fuq9dx98cspysyxxy5cpl6sawpywc';
+    : 'erd1qqqqqqqqqqqqqpgq0m9m5rhrr0efch3j3a0mxsp93mahf4y0pl6sq0ed08';
